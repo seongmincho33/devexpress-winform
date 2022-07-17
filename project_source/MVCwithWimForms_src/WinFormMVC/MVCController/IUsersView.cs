65fe9fcdebaefba230e912bf2eea97bd2ea4bf2b
seongmincho <seongmincho33@gmail.com>
2022-07-18 03:47:49 +0900

    simpleUpdate

---
+++ project_source/MVCwithWimForms_src/WinFormMVC/MVCController/IUsersView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WinFormMVC.Model;
+
+namespace WinFormMVC.Controller
+{
+    public interface IUsersView
+    {
+        void SetController(UsersController controller);
+        void ClearGrid();
+        void AddUserToGrid(User user);
+        void UpdateGridWithChangedUser(User user);
+        void RemoveUserFromGrid(User user);
+        string GetIdOfSelectedUserInGrid();
+        void SetSelectedUserInGrid(User user);
+        
+        string FirstName     { get; set; }
+        string LastName      { get; set; }
+        string ID            { get; set; }
+        string Department    { get; set; }
+        User.SexOfPerson Sex { get; set; }  
+        bool CanModifyID     {      set; }
+    }
+}
